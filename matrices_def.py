5ef8389ea2a2a14024d61130c65bb38806f2a953
Hui Zhang <huizhang54@gmail.com>
2026-01-04 22:26:05 -0500

    calculate the Axx matrix

--- matrices_def.py
+++ matrices_def.py
@@ -1,9 +1,10 @@
 import numpy as np
+from scipy.linalg import sqrtm, inv
 
 S = 50 # number of securities
 K = 5 # number of factors
 
-lambda = 0.001 # tcost multiplier on the covariance matrix
+lmbda = 0.001 # tcost multiplier on the covariance matrix
 
 rho = 0.05 # discount factor for the dynamic programming
 rho_bar = 1 - rho
@@ -33,6 +34,22 @@ if Sigma.shape[0] != S or Sigma.shape[1] != S:
     raise ValueError("Sigma is not a S x S matrix")
 
 # set the tcost matrix Lambda to be lambda times the Sigma matrix
-Lambda = lambda * Sigma
+Lambda = lmbda * Sigma
 Lambda_bar = (1.0/rho_bar) * Lambda
+
+# square root of Lambda_bar
+Lambda_bar_sqrt = sqrtm(Lambda_bar)
+
+# inverse of Lambda_bar
+Lambda_bar_inv = inv(Lambda_bar)
+
+# calculate rho_bar time gamma times square root of Lambda_bar times Sigma times square root of Lambda_bar
+Axx_term_1 = rho_bar * gamma * Lambda_bar_sqrt @ Sigma @ Lambda_bar_sqrt
+
+Axx_term_2 = 1./4. * (rho * rho * Lambda_bar * Lambda_bar + 2 * rho * gamma * Lambda_bar_sqrt @ Sigma @ Lambda_bar_sqrt + gamma * gamma * Lambda_bar_sqrt @ Sigma @ Lambda_bar_inv @ Sigma @ Lambda_bar_sqrt)
+
+Axx = sqrtm(Axx_term_1 + Axx_term_2) - 1./2. * (rho * Lambda_bar + gamma *  Sigma)
+
+# print the size of matrix Axx
+print("Axx.shape = ", Axx.shape)
 
